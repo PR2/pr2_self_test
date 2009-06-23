666e2cdada37c2325e24fc7f13c8ca831a047da3
tfoote <tfoote@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-06-23 23:43:34 +0000

    creating pr2 pr2_driver and map_building stacks and populating

--- pr2_etherCAT/scripts/reset_motors.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-import roslib
-roslib.load_manifest('mechanism_control')
-import rospy, sys
-import std_srvs.srv
-reset = rospy.ServiceProxy("reset_motors", std_srvs.srv.Empty)
-reset()
-
