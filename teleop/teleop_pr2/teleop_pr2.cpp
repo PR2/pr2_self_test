1c1999e3729d28ef5004c6657b8e73c30f8b8265
wattsk <wattsk@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-09-04 20:19:06 +0000

    Changed torso command topic

--- teleop/teleop_pr2/teleop_pr2.cpp
+++ teleop/teleop_pr2/teleop_pr2.cpp
@@ -41,7 +41,7 @@
 
 #include "std_msgs/Float64.h"
 
-#define TORSO_TOPIC "torso_lift_velocity_controller/set_command"
+#define TORSO_TOPIC "torso_lift_velocity_controller/command"
 #define HEAD_TOPIC "head_controller/command"
 
 using namespace std;
