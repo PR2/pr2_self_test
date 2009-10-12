92b3408dee71294b2be4ec53dd5a191c7ded43e7
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-10-12 23:02:21 +0000

    Made sure deadman_no_publish actually worked

--- teleop/teleop_pr2/teleop_pr2.cpp
+++ teleop/teleop_pr2/teleop_pr2.cpp
@@ -28,7 +28,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// Author: Kevin Watts
+///\author Kevin Watts
+///\brief Converts joystick commands on /joy to commands to PR2 base, spine, head
 
 #include <cstdlib>
 #include <unistd.h>
@@ -70,7 +71,7 @@ class TeleopPR2
   ros::Publisher torso_pub_;
   ros::Subscriber joy_sub_;
 
-  TeleopPR2(bool deadman_no_publish = false) : max_vx(0.6), max_vy(0.6), max_vw(0.8), max_vx_run(0.6), max_vy_run(0.6), max_vw_run(0.8), max_pan(2.7), max_tilt(1.4), min_tilt(-0.4), pan_step(0.02), tilt_step(0.015), deadman_no_publish_(deadman_no_publish)
+  TeleopPR2(bool deadman_no_publish = false) : max_vx(0.6), max_vy(0.6), max_vw(0.8), max_vx_run(0.6), max_vy_run(0.6), max_vw_run(0.8), max_pan(2.7), max_tilt(1.4), min_tilt(-0.4), pan_step(0.02), tilt_step(0.015), deadman_no_publish_(deadman_no_publish), deadman_(false)
   { }
 
   void init()
