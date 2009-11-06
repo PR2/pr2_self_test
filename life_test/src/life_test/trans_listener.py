65145470c35dd1d38c6d40fbe93cf0739c000d40
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2009-11-06 17:37:05 +0000

    Fixed transmission listener bug that didn't handle deadbands really close to joint wrap position

--- life_test/src/life_test/trans_listener.py
+++ life_test/src/life_test/trans_listener.py
@@ -298,9 +298,13 @@ class JointTransmissionListener():
         if self._up_ref is not None:
             if abs(position - self._up_ref) < self._deadband:
                 return True # Don't know b/c in deadband
+            if abs((self._wrap - position) - self._up_ref) < self._deadband:
+                return True # This checks that we're not really close to the flag on the other side of the wrap
         if self._down_ref is not None:
             if abs(position - self._down_ref) < self._deadband:
-                return True # Don't know b/c in deadband
+                return True 
+            if abs((self._wrap - position) - self._down_ref) < self._deadband:
+                return True 
 
         # Check limits
         if self._max is not None and position > self._max:
