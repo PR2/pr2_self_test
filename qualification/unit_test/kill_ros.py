d5bc69af15985c6b1bf5fc612e6f2fead519f635
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-02-25 22:05:17 +0000

    Adding kill ROS utilty to qualification system

---
+++ qualification/unit_test/kill_ros.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Kills all ROS processes. Used as utilty on qual station
+
+Script must be run as root, or:
+ * 'ckill kill --regex=.*ros.* must be able to run as user
+ * ckill needs to be modified not to check userid=0
+ * Command needs to be added to sudoers file
+"""
+
+import wx
+import sys, subprocess
+
+app = wx.PySimpleApp()
+
+dialog = wx.MessageDialog(None, "Are you are you want to kill ROS? This will kill the Qualification system and Test Manager", "Confirm Kill ROS", wx.OK|wx.CANCEL)
+if dialog.ShowModal() != wx.ID_OK:
+    sys.exit(1)
+
+cmd = ['ckill', 'kill', '--regex=.*ros.*']
+
+retcode = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+if retcode != 0:
+    wx.MessageBox("Unable to kill ROS. Retry by pressing \"Kill ROS\".", "Unable to kill ROS", wx.OK)
+else:
+    wx.MessageBox("ROS processes killed. Close any remaining windows", "ROS Killed", wx.OK)
+
