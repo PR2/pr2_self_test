7d93ca419f299cda979ba406d4693660690df962
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-04-23 02:42:20 +0000

    Moved test monitor code to library to make it easier to do listener load test. #3191

--- life_test/scripts/test_monitor.py
+++ life_test/scripts/test_monitor.py
@@ -34,7 +34,7 @@
 #
 
 ##\author Kevin Watts
-##\brief Loads listeners, monitors status of PR2 hardware tests
+##\brief Monitors status of PR2 hardware tests
 
 PKG = 'life_test'
 
@@ -44,170 +44,14 @@ roslib.load_manifest(PKG)
 import rospy
 
 from life_test.msg import TestStatus
-
-from diagnostic_msgs.msg import DiagnosticArray
-from std_srvs.srv import *
-import std_msgs.msg
+from life_test.pr2_hw_monitor import TestMonitor
 
 import traceback
 import sys
-
-TIMEOUT = 60 # If no heartbeat, shut down
-IGNORE_TIME = 30 # Ignore status for first few seconds
-
-def create_listener(params, listeners):
-    if not (params.has_key('type') and params.has_key('file')):
-        rospy.logerr('Params "type" and "file" weren\'t found!')
-        return False
-    
-    file = params['file']
-    type = params['type']
-
-    try:    
-        import_str = '%s.%s' % (PKG, file)
-        __import__(import_str)
-        pypkg = sys.modules[import_str]
-        listener_type = getattr(pypkg, type)
-    except:
-        rospy.logerr('Couldn\'t load listener %s from %s.\nException: %s' % (type, file, traceback.format_exc()))
-        return False
-    
-    try:
-        listener = listener_type()
-    except:
-        rospy.logerr('Listener %s failed to construct.\nException: %s' % (type, traceback.format_exc()))
-        return False
-                     
-    if not listener.create(params):
-        return False
-
-    listeners.append(listener)
-    return True
-
-##\todo Add slots here
-class StatusData:
-    __slots__ = ['level', 'messages', 'array']
-    def __init__(self):
-        self.level = TestStatus.RUNNING
-        self.messages = []
-        self.array = DiagnosticArray()
-
-# Listeners need: reset, halt, init, check_ok
-class TestMonitor:
-    def __init__(self):
-        rospy.init_node('test_monitor')
-
-        self._listeners = []
-
-        my_params = rospy.get_param("~")
-
-        self._listeners_ok = True
-
-        for ns, listener_param in my_params.iteritems():
-            if not create_listener(listener_param, self._listeners):
-                rospy.logerr('Listener failed to initialize. Namespace: %s' % ns)
-                self._listeners_ok = False
-
-
-        self._status_pub = rospy.Publisher('test_status', TestStatus)
-        self._diag_pub = rospy.Publisher('/diagnostics', DiagnosticArray)
-
-        self.reset_srv = rospy.Service('reset_test', Empty, self.reset_test)
-        self.halt_srv = rospy.Service('halt_test', Empty, self.halt_test)
-
-        self._heartbeat_time = rospy.get_time()
-        self._heartbeat_sub = rospy.Subscriber('/heartbeat', std_msgs.msg.Empty, self.on_heartbeat)
-        self._heartbeat_halted = False
-
-        self._was_ok = True
-        self._start_time = rospy.get_time() # Ignore failures for a bit after start
-
-    def on_heartbeat(self, msg):
-        self._heartbeat_time = rospy.get_time()
-
-    def reset_test(self, srv):
-        self._heartbeat_halted = False
-        self._was_ok = True
-        self._start_time = rospy.get_time()
-
-        for listener in self._listeners:
-            try:
-                listener.reset()
-            except:
-                rospy.logerr('Listener failed to reset!')
-        return EmptyResponse()
-
-    def halt_test(self, srv):
-        self._halt_listeners()
-
-        return EmptyResponse()
-
-    def _halt_listeners(self):
-        for listener in self._listeners:
-            try:
-                listener.halt()
-            except:
-                rospy.logerr('Listener failed to halt!')
-
-    def _check_status(self):
-        stat_data = StatusData()
-        
-        for listener in self._listeners:
-            try:
-                lvl, msg, diags = listener.check_ok()
-            except:
-                rospy.logerr('Listener failed to check status. %s' % traceback.format_exc())
-                stat, msg, diags = (TestStatus.ERROR, 'Error', None)
-
-            stat_data.level = max(stat_data.level, lvl)
-            if msg is not None and msg != '':
-                stat_data.messages.append(msg)
-            if diags is not None:
-                stat_data.array.status.extend(diags)
-        
-        if len(self._listeners) == 0:
-            stat_data.level = TestStatus.STALE
-            stat_data.messages = [ 'No listeners' ]
-            return stat_data
-
-        if not self._listeners_ok:
-            stat_data.level = TestStatus.ERROR
-            stat_data.messages = ['Listener Startup Error']
-
-        if rospy.get_time() - self._start_time > IGNORE_TIME and (self._was_ok and stat_data.level > 1):
-            self._halt_listeners()
-            rospy.logerr('Halted test after failure. Failure message: %s' % ', '.join(stat_data.messages))
-            self._was_ok = False
-            
-
-        if not self._heartbeat_halted and rospy.get_time() - self._heartbeat_time > TIMEOUT:
-            rospy.logerr('No heartbeat from Test Manager received, halting test')
-            self._halt_listeners()
-            self._heartbeat_halted = True
-
-
-        if self._heartbeat_halted:
-            stat_data.status = TestStatus.STALE
-            stat_data.messages = ['No heartbeat']
-            
-        return stat_data
-
-    def publish_status(self):
-        stat_data = self._check_status()
-
-        if len(stat_data.array.status) > 0:
-            stat_data.array.header.stamp = rospy.get_rostime()
-            self._diag_pub.publish(stat_data.array)
-
-        test_stat = TestStatus()
-        test_stat.test_ok = int(stat_data.level)
-        test_stat.message = ', '.join(stat_data.messages)
-        if test_stat.test_ok == TestStatus.RUNNING:
-            test_stat.message = 'OK'
-
-        self._status_pub.publish(test_stat)
+ 
 
 if __name__ == '__main__':
+    rospy.init_node('test_monitor')
     try:
         tm = TestMonitor()
         
@@ -220,3 +64,7 @@ if __name__ == '__main__':
     except:
         traceback.print_exc()
         rospy.logerr(traceback.format_exc())
+
+    sys.exit()
+
+    main()
