9df08caf43b9987cc36ab36ae087d23b0fa3885a
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-04-28 18:10:11 +0000

    Kill ROS now uses ckill from ROS tree

--- qualification/unit_test/kill_ros.py
+++ qualification/unit_test/kill_ros.py
@@ -3,13 +3,16 @@
 import wx
 import sys, subprocess
 
+import roslib; roslib.load_manifest('qualification')
+
 app = wx.PySimpleApp()
 
-dialog = wx.MessageDialog(None, "Are you are you want to kill ROS? This will kill the Qualification system and Test Manager", "Confirm Kill ROS", wx.OK|wx.CANCEL)
+dialog = wx.MessageDialog(None, "Are you are you want to kill ROS? This will kill the Qualification system and Test Manager. Press \"Cancel\" to abort.", "Confirm Kill ROS", wx.OK|wx.CANCEL)
 if dialog.ShowModal() != wx.ID_OK:
     sys.exit(1)
 
-cmd = ['ckill', 'kill', '--sig=9']
+whitelist_path = os.path.join(roslib.packages.get_pkg_dir('ckill'), 'whitelist')
+cmd = ['rosrun', 'ckill', 'ckill.py', 'kill', '--sig=9', '--whitelist=%s' % whitelist_path]
 
 retcode = subprocess.call(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
