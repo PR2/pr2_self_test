5c792a3843e25af598e36c410482e094da73316a
vpradeep <vpradeep@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-05-12 00:43:51 +0000

    Fixing typos.  Adding scatterplots for preburn

---
+++ pr2_bringup_tests/calibration_bringup/preburn/preburn_view_forearm_scatter.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+rosrun pr2_calibration_estimation post_process.py /u/pr2admin/preburn/cal_measurements.bag /u/pr2admin/preburn `rospack find pr2_calibration_launch`/view_results/forearm_config.yaml
